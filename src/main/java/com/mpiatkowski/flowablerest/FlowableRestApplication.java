8d51a8468354376ff4d3b751f17a994d5ff2dbdf
Mateusz Piatkowski <mpiatkowski@box.com>
2023-08-24 10:24:45 +0200

    init commit

---
+++ src/main/java/com/mpiatkowski/flowablerest/FlowableRestApplication.java
@@ -0,0 +1,13 @@
+package com.mpiatkowski.flowablerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpiatkowski/flowablerest/FlowableRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpiatkowski.flowablerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
